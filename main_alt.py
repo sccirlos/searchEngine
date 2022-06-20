d28d33013e04fde872f8520afd02885ffc4fddde
sccirlos <sccirlos@gmail.com>
2022-06-19 19:30:11 -0500

    updating main_alt

--- main_alt.py
+++ main_alt.py
@@ -4,21 +4,13 @@
 # Date Range: Summer I 2022                                                                      #
 # Short Description: Web search engine implementation using an inverted index tables.            #
 ##################################################################################################
-# import libraries for UI
-import sys
-from PyQt5.uic import mainwindow
-from PyQt5.uic import searchResultsUI
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget
 
-
-
-# Import necessary libraries for search engine
+# Import necessary libraries
 import os
 import zipfile
 from bs4 import BeautifulSoup
 
-# Unzip Jan.zip if necessary.
+# Unzip Jan.zip if necessary. "Folder already there..." prints if the extracted folder is already present.
 def unzipContents():
     if 'Jan' not in os.listdir():
         with zipfile.ZipFile('cheDoc.zip', 'r') as zip_ref:
@@ -28,14 +20,15 @@ def unzipContents():
 
 # Traverse HTML files, returns a inverted index hash table.
 def traverseHTML(htmlFiles):
-    stop_words = ['a', 'an', 'the', 'of']
+
+    stop_words = ['a', 'an', 'the', 'of'] # removal of stop words
     invertedIndexDic = {}
     docTable = {}
-    currentSpotChecker = range(len(htmlFiles))
-    location = 0
-    corpus = []
+
+    # Begin creating inverted index and document list (stored in docTable) hash maps.
     for item in htmlFiles:
-        currentSpot = htmlFiles.index(item)
+
+        # Initialize the creation of the document list.
         docTable[item] = {
             'doc vec length': 0,
             'max freq': 0,
@@ -51,30 +44,47 @@ def traverseHTML(htmlFiles):
 
             # Strip text of numerical and stop words
             currentFileText = [word for word in currentFileText if word.isalpha() and word not in stop_words]
+
             # Tiny hack: convert current text to file
             for word in list(dict.fromkeys(currentFileText)):
                 if word not in invertedIndexDic.keys():
                     invertedIndexDic[word] = {
                         'df': 1,
-                        'link': [[item, currentFileText.count(word)]]
+                        'link': [[item, currentFileText.count(word), [index for index, item in enumerate(currentFileText) if item == word]]]
                     }
                 else:
                     invertedIndexDic[word]['df'] += 1
                     if item not in invertedIndexDic[word]['link']:
-                        invertedIndexDic[word]['link'].append([item, currentFileText.count(word)])
-                    #else:
-                    #    invertedIndexDic[word]['link'][1 += 1
+                        invertedIndexDic[word]['link'].append([item, currentFileText.count(word),[index for index, item in enumerate(currentFileText) if item == word]])
 
-                    # invertedIndexDic[word]['link'].append(1)
+            docTable[item]['max freq'] = max([list(value['link'])[0][1] for key, value in invertedIndexDic.items() if list(value['link'])[0][0] == item])
+
+            for entry in [value['link'] for key, value in invertedIndexDic.items()]:
+                for subEntry in entry:
+                    if docTable[item]['max freq'] < subEntry[1] and subEntry[0] == item:
+                        docTable[item]['max freq'] = subEntry[1]
+                    #print(subEntry)
+            #print([value['link'] for key, value in invertedIndexDic.items()])
+            print(":::::")
+
     return invertedIndexDic, docTable
+
+def phrsalQuery(query):
+    if "or" or "and" or "but" in query.split():
+        print("boolean search")
+    #return s_
 
 def webSearch(doc):
     print("Now the search beings:")
     keysearch = input("enter a search key=>")
     while (keysearch != ""):
-        for key, value in doc.items():
-            if " " + keysearch + " " in value:
-                print("found a match: ./cheDoc/"+str(key))
+        keysearch = keysearch.split()
+        phrsalQuery(keysearch)
+        for thing in keysearch:
+            print(doc[0][thing]['link'])
+        #for key, value in doc.items():
+        #    if " " + keysearch + " " in value:
+        #        print("found a match: ./cheDoc/"+str(key))
         keysearch = input("enter a search key=>")
     print("Bye")
 
@@ -90,9 +100,9 @@ if __name__ == '__main__':
     # Store HTML files into a Dic
     completeDocumentsDic = traverseHTML(allHTMLFiles)
     #print(completeDocumentsDic)
-    for key, value in completeDocumentsDic[0].items():
+    for key, value in completeDocumentsDic[1].items():
         print(str(key) + " " + str(value.items()))
-    #print(completeDocumentsDic)
+    print(completeDocumentsDic[0])
     #print(completeDocumentsDic[1])
     #print(completeDocumentsDic[0]['cat'])
     # Engine
